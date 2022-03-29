2cfd0dd8a9e2e6f528de1039bef2286d255eecd4
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-30 03:50:36 +0530

    Create leftbar and rightbar

---
+++ lib/screens/home.dart
@@ -0,0 +1,32 @@
+import 'package:bmi_cal/constants/app_constants.dart';
+import 'package:bmi_cal/widgets/left_bar.dart';
+import 'package:bmi_cal/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height:20),
+          ]
+        )
+      )
+    );
+  }
+}
