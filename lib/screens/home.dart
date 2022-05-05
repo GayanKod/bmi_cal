b7710ce4e8ed57ec89755b3603fdb9b7706ccd39
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-05-05 19:59:48 +0530

    Update home.dart
    Add bar design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -73,7 +73,28 @@ class _HomeScreenState extends State<HomeScreen> {
               ] 
 
 
-            )
+            ),
+            SizedBox(height: 30,),
+            Container(
+                child: Text("Calculate", style:TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: accentHexColor ),),
+            ),
+            SizedBox(height: 50,),
+            Container(
+              child: Text("10", style: TextStyle(fontSize:90, color:accentHexColor),), 
+            ),
+
+            SizedBox(height: 30,),
+            Container(
+                child: Text("Normal weight", style: TextStyle(fontSize:32, fontWeight:FontWeight.w400, color:accentHexColor),),
+            ),
+
+            SizedBox(height: 10,),
+            LeftBar(barWidth: 40,),
+            SizedBox(height: 20,),
+            LeftBar(barWidth: 70,),
+            SizedBox(height: 20,),
+            LeftBar(barWidth: 40,),
+
           ]
         )
       )
